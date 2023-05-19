c7d3a7683ffed41a360059eb1e2662246ca4f586
Imran <imtiazimran@outlook.com>
2023-05-19 19:07:24 +0600

    toy collection updated

--- index.js
+++ index.js
@@ -10,6 +10,52 @@ app.use(cors());
 app.use(express.json())
 
 
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@cluster0.5ujci4u.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const allToys = client.db('toyGalaxy').collection('toyCollection')
+
+
+    const marvelCollection = client.db('toyGalaxy').collection('marvel')
+    const dcCollection = client.db('toyGalaxy').collection('dc')
+    const starWarsCollection = client.db('toyGalaxy').collection('starWars')
+    
+
+
+    app.get('/allToys', async(req, res) =>{
+        const result = await allToys.find().toArray()
+        res.send(result)
+    })
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+
+
 app.get('/', (req, res) =>{
     res.send("welcome to Our Toy Shop")
 })
